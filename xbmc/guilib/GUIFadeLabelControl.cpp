64452d293c97851b765b7b72e46d89ce3124cc7e
anaconda <anaconda@menakite.eu>
2014-09-11 21:30:43 +0200

    Disable autoscrolling while on screensaver and while opening streams.

--- xbmc/guilib/GUIFadeLabelControl.cpp
+++ xbmc/guilib/GUIFadeLabelControl.cpp
@@ -10,6 +10,7 @@
 
 #include "GUIMessage.h"
 #include "utils/Random.h"
+#include "Application.h"
 
 using namespace KODI::GUILIB;
 
@@ -105,7 +106,7 @@ void CGUIFadeLabelControl::Process(unsigned int currentTime, CDirtyRegionList &d
     MarkDirtyRegion();
   }
 
-  if (m_infoLabels.size() > 1 || !m_shortText)
+  if ((m_infoLabels.size() > 1 || !m_shortText) && !g_application.ScreenSaverDisablesAutoScrolling())
   { // have scrolling text
     bool moveToNextLabel = false;
     if (!m_scrollOut)
