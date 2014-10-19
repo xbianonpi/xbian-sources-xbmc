46ef8f5b94a6b2902e7a9b21cc30b533d24707a7
Anssi Hannula <anssi@xbmc.org>
2014-10-19 21:36:44 +0300

    [AE] ALSA: Add ALSADeviceMonitor for monitoring card removals/additions
    (cherry picked from commit 473c2ce4da9180ac7b6c5e32c7ebc1a2c238b942)
    Signed-off-by: Matus Kral <matuskral@me.com>

--- xbmc/cores/AudioEngine/Sinks/AESinkALSA.cpp
+++ xbmc/cores/AudioEngine/Sinks/AESinkALSA.cpp
@@ -1103,6 +1103,10 @@ bool CAESinkALSA::OpenPCMDevice(const std::string &name, const std::string &para
 
 void CAESinkALSA::EnumerateDevicesEx(AEDeviceInfoList &list, bool force)
 {
+#if HAVE_LIBUDEV
+  m_deviceMonitor.Start();
+#endif
+
   /* ensure that ALSA has been initialized */
   snd_lib_error_set_handler(sndLibErrorHandler);
   if(!snd_config || force)
@@ -1617,5 +1621,9 @@ void CAESinkALSA::sndLibErrorHandler(const char *file, int line, const char *fun
   }
   va_end(arg);
 }
+
+#if HAVE_LIBUDEV
+CALSADeviceMonitor CAESinkALSA::m_deviceMonitor; // ARGH
+#endif
 
 #endif
