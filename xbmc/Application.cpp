53c742a539b751c5d41baff46fb965d3348786bf
anaconda <anaconda@menakite.eu>
2014-12-27 01:39:15 +0100

    [guilib] Also disable autoscrolling while opening streams.
    Signed-off-by: Matus Kral <matuskral@me.com>
    
    Conflicts:
    	xbmc/Application.cpp

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -5082,12 +5082,10 @@ bool CApplication::NotifyActionListeners(const CAction &action) const
 
 bool CApplication::ScreenSaverDisablesAutoScrolling()
 {
-  // This 'if' clause is completely unnecessary, but by not touching the 'return'
-  // below this patch becomes more easily maintainable.
-  if (GetCecStandby())
-    return true;
-
-  return IsInScreenSaver() && m_screenSaver &&
+  bool onBlackDimScreenSaver = IsInScreenSaver() && m_screenSaver &&
     (m_screenSaver->ID() == "screensaver.xbmc.builtin.black" ||
      m_screenSaver->ID() == "screensaver.xbmc.builtin.dim");
+  bool openingStreams = m_pPlayer->IsPlaying() && g_windowManager.IsWindowActive(WINDOW_DIALOG_BUSY);
+
+  return onBlackDimScreenSaver || openingStreams;
 }
