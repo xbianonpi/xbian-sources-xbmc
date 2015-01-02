05c7f51b62f9bbd5ebff4ce90c4c38a9905c6721
Rainer Hochecker <fernetmenta@online.de>
2015-01-02 10:02:09 +0100

    renderer: drop old tempfix firstflippage, did not work anyway because RendererHandlesPresent returned always true
    Upstream-commit: f4e5d469dbe3bc12471d1a4ce6783d09a1cc9e89
    
        (cherry picked from commit 23f4347c2915bb4032f4353d06cd673fd0888762)
        Signed-off-by: Matus Kral <matuskral@me.com>

--- xbmc/guilib/GUIVideoControl.cpp
+++ xbmc/guilib/GUIVideoControl.cpp
@@ -43,8 +43,7 @@ void CGUIVideoControl::Process(unsigned int currentTime, CDirtyRegionList &dirty
   g_renderManager.FrameMove();
 
   // TODO Proper processing which marks when its actually changed. Just mark always for now.
-  if (g_renderManager.IsGuiLayer())
-    MarkDirtyRegion();
+  MarkDirtyRegion();
 
   CGUIControl::Process(currentTime, dirtyregions);
 }
