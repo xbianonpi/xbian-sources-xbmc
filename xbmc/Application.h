45386a20d1f0d4250f04383f6d082ce2d72b9264
Matus Kral <matuskral@me.com>
2015-03-28 21:28:12 +0100

    Use timer for VT changes update process

--- xbmc/Application.h
+++ xbmc/Application.h
@@ -431,6 +431,7 @@ protected:
   CStopWatch m_frameTime;
   CStopWatch m_navigationTimer;
   CStopWatch m_slowTimer;
+  CStopWatch m_slowTimerVT;
   CStopWatch m_shutdownTimer;
 
   bool m_bInhibitIdleShutdown;
