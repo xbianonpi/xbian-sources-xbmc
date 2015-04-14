78f03bdf6c96fec591c41a2feebed69872138622
popcornmix <popcornmix@gmail.com>
2015-04-14 17:51:27 +0100

    [mmalcodec] Avoid double rate deinterlace when not fullscreen or with OSD
    (cherry picked from commit 61c7a1b896aeecbcf6f5fc1ea29429bf23d96630)
    Signed-off-by: Matus Kral <matuskral@me.com>

--- xbmc/cores/VideoRenderers/MMALRenderer.cpp
+++ xbmc/cores/VideoRenderers/MMALRenderer.cpp
@@ -32,6 +32,7 @@
 #include "windowing/WindowingFactory.h"
 #include "cores/dvdplayer/DVDCodecs/Video/MMALCodec.h"
 #include "xbmc/Application.h"
+#include "guilib/GUIWindowManager.h"
 
 #define CLASSNAME "CMMALRenderer"
 
@@ -576,7 +577,10 @@ bool CMMALRenderer::Supports(ESCALINGMETHOD method)
 
 EINTERLACEMETHOD CMMALRenderer::AutoInterlaceMethod()
 {
-  return VS_INTERLACEMETHOD_MMAL_ADVANCED;
+  if (!g_graphicsContext.IsFullScreenVideo() || g_windowManager.HasDialogOnScreen())
+    return VS_INTERLACEMETHOD_MMAL_ADVANCED_HALF;
+  else
+    return VS_INTERLACEMETHOD_MMAL_ADVANCED;
 }
 
 void CMMALRenderer::SetVideoRect(const CRect& InSrcRect, const CRect& InDestRect)
