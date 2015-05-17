9a4a9ca96011e31449ffaaa3dc4b74c432d13339
Matus Kral <matuskral@me.com>
2015-05-17 15:18:15 +0200

    remove unneeded bcm_host.h and allow compilation for GLES2 platforms
    Upstream-commit: e9f5f45d685da4233404c34a18e354afae6a3b3e

--- xbmc/screensavers/gles/triangle2.c
+++ xbmc/screensavers/gles/triangle2.c
@@ -37,8 +37,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <unistd.h>
 #include <sys/time.h>
 
-#include "bcm_host.h"
-
 #include "triangle2.h"
 
 #define min(a,b) ((a)<(b)?(a):(b)) 
