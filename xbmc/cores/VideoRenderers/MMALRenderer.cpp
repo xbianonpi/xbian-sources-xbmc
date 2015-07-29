c179d4d0999095966eef6ce43657ee7fda4decdd
popcornmix <popcornmix@gmail.com>
2015-07-29 14:06:17 +0100

    Wait for vsync before submitting to mmal when display sync is disabled
    This avoids an issue where video occasionally goes stuttery after a seek, until the next pause/play or seek.
    The issue is when display sync is disabled, and framerate of video matches display, and render times are coincident with vsync
    you find that depending on timestamp/scheduling jitter, you may or may not get an update each vsync resulting in stuttery video.
    
    Some scheme to force render times to be dependent on vsync is required. We do this by blocking in RenderUpdate unto next vsync.

--- xbmc/cores/VideoRenderers/MMALRenderer.cpp
+++ xbmc/cores/VideoRenderers/MMALRenderer.cpp
@@ -208,6 +208,7 @@ CMMALRenderer::CMMALRenderer()
   m_bMMALConfigured = false;
   m_iYV12RenderBuffer = 0;
   m_sharpness = -2.0f;
+  m_vsyncCount = ~0;
 }
 
 CMMALRenderer::~CMMALRenderer()
@@ -357,6 +358,24 @@ void CMMALRenderer::Update()
   ManageDisplay();
 }
 
+void CMMALRenderer::SubmitFrame(MMAL_BUFFER_HEADER_T *buffer)
+{
+  if (!CSettings::Get().GetBool("videoplayer.usedisplayasclock"))
+  {
+    if (fabs(m_fps - g_graphicsContext.GetFPS()) < 1e-2)
+    {
+      #if defined(MMAL_DEBUG_VERBOSE)
+      CLog::Log(LOGDEBUG, "%s::%s - buffer:%p vfps:%.3f dfps:%.3f vsync:%d dsync:%d", CLASSNAME, __func__, buffer, m_fps, g_graphicsContext.GetFPS(), m_vsyncCount, CSettings::Get().GetBool("videoplayer.usedisplayasclock"));
+      #endif
+      m_vsyncCount = g_RBP.WaitVsync(m_vsyncCount) + 1;
+      #if defined(MMAL_DEBUG_VERBOSE)
+      CLog::Log(LOGDEBUG, "%s::%s - done vsync:%d", CLASSNAME, __func__, m_vsyncCount);
+      #endif
+    }
+  }
+  mmal_port_send_buffer(m_vout_input, buffer);
+}
+
 void CMMALRenderer::RenderUpdate(bool clear, DWORD flags, DWORD alpha)
 {
   CSingleLock lock(m_sharedSection);
@@ -402,7 +421,7 @@ void CMMALRenderer::RenderUpdate(bool clear, DWORD flags, DWORD alpha)
         return;
       omvb->Acquire();
       omvb->mmal_buffer->flags |= MMAL_BUFFER_HEADER_FLAG_USER1 | MMAL_BUFFER_HEADER_FLAG_USER2;
-      mmal_port_send_buffer(m_vout_input, omvb->mmal_buffer);
+      SubmitFrame(omvb->mmal_buffer);
     }
     else
       CLog::Log(LOGDEBUG, "%s::%s - No buffer to update", CLASSNAME, __func__);
@@ -417,7 +436,7 @@ void CMMALRenderer::RenderUpdate(bool clear, DWORD flags, DWORD alpha)
         return;
       // sanity check it is not on display
       buffer->mmal_buffer->flags |= MMAL_BUFFER_HEADER_FLAG_USER1 | MMAL_BUFFER_HEADER_FLAG_USER2;
-      mmal_port_send_buffer(m_vout_input, buffer->mmal_buffer);
+      SubmitFrame(buffer->mmal_buffer);
     }
     else
       CLog::Log(LOGDEBUG, "%s::%s - No buffer to update", CLASSNAME, __func__);
