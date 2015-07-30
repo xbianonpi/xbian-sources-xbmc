EsMaSol/xbmc

player: implement SeekTimeRelative, disable accurate seeking for relative seeks

xbmc/ApplicationPlayer.cpp
@@ -264,6 +264,20 @@ void CApplicationPlayer::SeekTime(int64_t iTime)
     player->SeekTime(iTime);
 }
 
+void CApplicationPlayer::SeekTimeRelative(int64_t iTime)
+{
+  std::shared_ptr<IPlayer> player = GetInternal();
+  if (player)
+  {
+    // use relative seeking if implemented by player
+    if (!player->SeekTimeRelative(iTime))
+    {
+      int64_t abstime = player->GetTime() + iTime;
+      player->SeekTime(abstime);
+    }
+  }
+}
+
 std::string CApplicationPlayer::GetPlayingTitle()
 {
   std::shared_ptr<IPlayer> player = GetInternal();

xbianonpi/xbian-sources-xbmc

(cherry picked from commit f269d13c7fb24838cd18021778a5ced37355b2ae)
Signed-off-by: Matus Kral <matuskral@me.com>

xbmc/ApplicationPlayer.cpp
@@ -243,6 +243,20 @@ void CApplicationPlayer::SeekTime(int64_t iTime)
     player->SeekTime(iTime);
 }
 
+void CApplicationPlayer::SeekTimeRelative(int64_t iTime)
+{
+  boost::shared_ptr<IPlayer> player = GetInternal();
+  if (player)
+  {
+    // use relative seeking if implemented by player
+    if (!player->SeekTimeRelative(iTime))
+    {
+      int64_t abstime = player->GetTime() + iTime;
+      player->SeekTime(abstime);
+    }
+  }
+}
+
 std::string CApplicationPlayer::GetPlayingTitle()
 {
   boost::shared_ptr<IPlayer> player = GetInternal();