xbmc/xbmc

dvdplayer: use time of frame on screen for displayed time

xbmc/ApplicationPlayer.cpp
@@ -296,6 +296,15 @@ int64_t CApplicationPlayer::GetTime() const
     return 0;
 }
 
+int64_t CApplicationPlayer::GetDisplayTime() const
+{
+  std::shared_ptr<IPlayer> player = GetInternal();
+  if (player)
+    return player->GetDisplayTime();
+  else
+    return 0;
+}
+
 bool CApplicationPlayer::IsCaching() const
 {
   std::shared_ptr<IPlayer> player = GetInternal();

xbianonpi/xbian-sources-xbmc

(cherry picked from commit 43b6cf683abf16bc41e9b7f624a678c285c85c97)
Signed-off-by: Matus Kral <matuskral@me.com>

xbmc/ApplicationPlayer.cpp
@@ -261,6 +261,15 @@ int64_t CApplicationPlayer::GetTime() const
     return 0;
 }
 
+int64_t CApplicationPlayer::GetDisplayTime() const
+{
+  boost::shared_ptr<IPlayer> player = GetInternal();
+  if (player)
+    return player->GetDisplayTime();
+  else
+    return 0;
+}
+
 bool CApplicationPlayer::IsCaching() const
 {
   boost::shared_ptr<IPlayer> player = GetInternal();