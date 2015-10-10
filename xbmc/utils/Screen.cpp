2785772aecd9d7dbef6ad708ad5feef19cd2f86a
Matus Kral <matuskral@me.com>
2015-10-10 06:41:51 +0200

    Screen: don't stop/start refclock on pause/unpause. Still stop/start it for all other actions.

--- xbmc/utils/Screen.cpp
+++ xbmc/utils/Screen.cpp
@@ -58,10 +58,12 @@ void CScreen::Announce(AnnouncementFlag flag, const char *sender, const char *me
     g_screen.SetOn();
   else if (flag == GUI && !strcmp(sender, "xbmc") && !strcmp(message, "OnScreensaverActivated") && CSettings::Get().GetString("screensaver.mode") == "screensaver.xbmc.builtin.black")
     g_screen.SetOff();
+/*
   else if (flag == Player && !strcmp(sender, "xbmc") && !strcmp(message, "OnPlay"))
     g_VideoReferenceClock.Start();
   else if (flag == Player && !strcmp(sender, "xbmc") && !strcmp(message, "OnPause"))
     g_VideoReferenceClock.Stop();
+*/
 }
 
 void CScreen::ScreenPowerOff(bool doBlank)
