0dedfbba22f0c5483f3246169213e2a99df5c6b4
Matus Kral <matuskral@me.com>
2015-12-23 12:44:21 +0100

    [XBian] cec: Increase interval for events check to 20ms (5ms is useless)

--- xbmc/peripherals/devices/PeripheralCecAdapter.cpp
+++ xbmc/peripherals/devices/PeripheralCecAdapter.cpp
@@ -387,7 +387,7 @@ void CPeripheralCecAdapter::Process(void)
       ProcessStandbyDevices();
 
     if (!m_bStop)
-      CThread::Sleep(5ms);
+      CThread::Sleep(20ms);
   }
 
   m_queryThread->StopThread(true);
