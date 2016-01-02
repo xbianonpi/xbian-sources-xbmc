07a584b94e1828a8ded750da386cda11e5018892
popcornmix <popcornmix@gmail.com>
2016-01-02 20:19:23 +0000

    [debug] Log when playback start takes too long

--- xbmc/application/Application.cpp
+++ xbmc/application/Application.cpp
@@ -2791,6 +2791,7 @@ bool CApplication::OnMessage(CGUIMessage& message)
 
   case GUI_MSG_PLAYBACK_STARTED:
     {
+      int64_t Start = CurrentHostCounter();
 #ifdef TARGET_DARWIN_EMBEDDED
       // @TODO move this away to platform code
       CDarwinUtils::SetScheduling(GetComponent<CApplicationPlayer>()->IsPlayingVideo());
@@ -2843,6 +2844,10 @@ bool CApplication::OnMessage(CGUIMessage& message)
       if (!m_itemCurrentFile->IsLiveTV() ||
           (!appPlayer->IsPlayingVideo() && !appPlayer->IsPlayingAudio()))
         CGUIDialogBusy::WaitOnEvent(m_playerEvent);
+
+      float duration = (CurrentHostCounter()-Start) * 1e-9;
+      if (duration > 0.1f)
+        CLog::LogF(LOGWARNING, "Suspiciously long time to handle GUI_MSG_PLAYBACK_STARTED ({:2f}s)", duration);
 
       return true;
     }
