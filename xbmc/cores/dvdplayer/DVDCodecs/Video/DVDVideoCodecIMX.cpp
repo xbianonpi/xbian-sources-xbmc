cc0eddb4fef31eff676f3a92cfec0c1939748dc7
Matus Kral <matuskral@me.com>
2016-02-01 04:30:54 +0100

    [DVDVideoCodecIMX] allow first frame to pass to VideoPlayer to pass media info

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecIMX.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecIMX.cpp
@@ -1104,13 +1104,15 @@ bool CDVDVideoCodecIMX::GetPicture(DVDVideoPicture* pDvdVideoPicture)
   previous = current;
 #endif
 
-  m_frameCounter++;
-
   if (m_dropState)
   {
     pDvdVideoPicture->iFlags = DVP_FLAG_DROPPED;
-    SAFE_RELEASE(m_currentBuffer);
     m_dropState = false;
+  }
+
+  if (m_frameCounter++ && pDvdVideoPicture->iFlags == DVP_FLAG_DROPPED)
+  {
+    SAFE_RELEASE(m_currentBuffer);
     return true;
   }
 
@@ -1155,7 +1157,7 @@ void CDVDVideoCodecIMX::SetDropState(bool bDrop)
 
   if (m_dropRequest != bDrop)
   {
-    m_dropRequest = bDrop;
+    m_dropRequest = bDrop && !!m_frameCounter;
 #ifdef TRACE_FRAMES
     CLog::Log(LOGDEBUG, "%s : %d\n", __FUNCTION__, bDrop);
 #endif
