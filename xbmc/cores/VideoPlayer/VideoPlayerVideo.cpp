94ea1c70976e9947c8404f2b6b16b54954bc2c3e
popcornmix <popcornmix@gmail.com>
2016-03-05 19:09:13 +0000

    debug: Include drop/skip counts in log

--- xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
@@ -252,6 +252,7 @@ void CVideoPlayerVideo::CloseStream(bool bWaitForBuffers)
     m_picture.videoBuffer->Release();
     m_picture.videoBuffer = nullptr;
   }
+  CLog::Log(LOGDEBUG, "CVideoPlayerVideo::CloseStream - drop:{} skip:{}", m_iDroppedFrames, m_renderManager.GetSkippedFrames());
 }
 
 bool CVideoPlayerVideo::AcceptsData() const
