8d5a0dcc2992a94009a193ef87f59b47d61bdb0d
popcornmix <popcornmix@gmail.com>
2016-07-19 20:39:18 +0100

    mmalrender: Add sharpness control

--- xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/MMALRenderer.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/MMALRenderer.cpp
@@ -579,6 +579,7 @@ CMMALRenderer::CMMALRenderer() : CThread("MMALRenderer"), m_processThread(this, 
   m_frameInterval = 0.0;
   m_frameIntervalDiff = 1e5;
   m_vsync_count = ~0U;
+  m_sharpness = -2.0f;
   m_vout_width = 0;
   m_vout_height = 0;
   m_vout_aligned_width = 0;
@@ -1018,6 +1019,15 @@ void CMMALRenderer::RenderUpdate(int index, int index2, bool clear, unsigned int
     goto exit;
   }
 
+  // if sharpness setting has changed, we should update it
+  if (m_sharpness != m_videoSettings.m_Sharpness)
+  {
+    m_sharpness = m_videoSettings.m_Sharpness;
+    char command[80], response[80];
+    sprintf(command, "scaling_sharpness %d", ((int)(50.0f * (m_sharpness + 1.0f) + 0.5f)));
+    vc_gencmd(response, sizeof response, command);
+  }
+
   if (omvb && omvb->m_state == MMALStateBypass)
   {
     // dummy buffer from omxplayer
@@ -1155,7 +1165,8 @@ bool CMMALRenderer::Supports(ERENDERFEATURE feature)
       feature == RENDERFEATURE_ZOOM            ||
       feature == RENDERFEATURE_ROTATION        ||
       feature == RENDERFEATURE_VERTICAL_SHIFT  ||
-      feature == RENDERFEATURE_PIXEL_RATIO)
+      feature == RENDERFEATURE_PIXEL_RATIO     ||
+      feature == RENDERFEATURE_SHARPNESS)
     return true;
 
   return false;
