54668fc580813ad360772598a4a193db2aa7d1a6
popcornmix <popcornmix@gmail.com>
2016-11-11 15:53:53 +0000

    stereoscopicmanager: fixups for rbp

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/MMALCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/MMALCodec.cpp
@@ -333,6 +333,11 @@ bool CMMALVideo::SendCodecConfigData()
   return true;
 }
 
+bool CMMALVideo::SupportsExtention()
+{
+  return CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_VIDEOPLAYER_SUPPORTMVC);
+}
+
 bool CMMALVideo::Open(CDVDStreamInfo &hints, CDVDCodecOptions &options)
 {
   CSingleLock lock(m_sharedSection);
@@ -384,12 +389,12 @@ bool CMMALVideo::Open(CDVDStreamInfo &hints, CDVDCodecOptions &options)
       m_codingType = MMAL_ENCODING_H264;
       m_pFormatName = "mmal-h264";
       if ((hints.codec_tag == MKTAG('M', 'V', 'C', '1') || hints.codec_tag == MKTAG('A', 'M', 'V', 'C')) &&
-        CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(CSettings::SETTING_VIDEOPLAYER_SUPPORTMVC))
+          SupportsExtention())
       {
         m_codingType = MMAL_ENCODING_MVC;
         m_pFormatName= "mmal-mvc";
         if (hints.stereo_mode == "mono")
-          hints.stereo_mode = "mvc_lr";
+          hints.stereo_mode = "block_lr";
       }
     break;
     case AV_CODEC_ID_H263:
