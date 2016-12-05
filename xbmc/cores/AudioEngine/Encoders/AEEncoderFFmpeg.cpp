ee5549b70e77db0b82a93f4bc09ba0c2948005dc
popcornmix <popcornmix@gmail.com>
2016-12-05 12:08:02 +0000

    test: Add advancedsetting for AC3 bitrate

--- xbmc/cores/AudioEngine/Encoders/AEEncoderFFmpeg.cpp
+++ xbmc/cores/AudioEngine/Encoders/AEEncoderFFmpeg.cpp
@@ -10,6 +10,10 @@
 #define EAC3_ENCODE_BITRATE 768000
 #define DTS_ENCODE_BITRATE 1411200
 
+#include "ServiceBroker.h"
+#include "settings/Settings.h"
+#include "settings/SettingsComponent.h"
+#include "settings/AdvancedSettings.h"
 #include "cores/AudioEngine/Encoders/AEEncoderFFmpeg.h"
 
 #include "cores/AudioEngine/Utils/AEUtil.h"
@@ -107,7 +111,7 @@ bool CAEEncoderFFmpeg::Initialize(AEAudioFormat& format, bool allow_planar_input
   {
     m_CodecName = "AC3";
     m_CodecID = AV_CODEC_ID_AC3;
-    m_BitRate = AC3_ENCODE_BITRATE;
+    m_BitRate = CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_ac3Gain ? CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_ac3Bitrate : AC3_ENCODE_BITRATE;
     codec = avcodec_find_encoder(m_CodecID);
   }
 
