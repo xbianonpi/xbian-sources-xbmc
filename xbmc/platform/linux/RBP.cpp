43d7db4f192730d3d4798ccdc0a14f722dba3a54
popcornmix <popcornmix@gmail.com>
2016-12-06 20:14:46 +0000

    testing: Try minimal libass cache size on all devices
    No reports of a change in behaviour with lower settings so lets try minimal

--- xbmc/platform/linux/RBP.cpp
+++ xbmc/platform/linux/RBP.cpp
@@ -91,7 +91,7 @@ void CRBP::InitializeSettings()
   if (m_initialized && CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_cacheMemSize == ~0U)
     CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_cacheMemSize = m_arm_mem < 256 ? 1024 * 1024 * 2 : 1024 * 1024 * 20;
   if (m_initialized && CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_libAssCache == ~0U)
-    CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_libAssCache = m_arm_mem < 256 ? 21 : m_arm_mem < 512 ? 42 : 96;
+    CServiceBroker::GetSettingsComponent()->GetAdvancedSettings()->m_libAssCache = 21; //m_arm_mem < 256 ? 21 : m_arm_mem < 512 ? 42 : 96;
 }
 
 bool CRBP::Initialize()
