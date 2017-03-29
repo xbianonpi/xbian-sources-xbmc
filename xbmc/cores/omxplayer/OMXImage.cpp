3b590b815a5f3f5f5b3dc3472df209d560429203
popcornmix <popcornmix@gmail.com>
2017-03-29 18:58:10 +0100

    OMXImage: Add back in some debug/error logging

--- xbmc/cores/omxplayer/OMXImage.cpp
+++ xbmc/cores/omxplayer/OMXImage.cpp
@@ -190,19 +190,27 @@ bool COMXImage::CreateThumb(const std::string& srcFile, const XFILE::auto_buffer
   int orientation = additional_info == "flipped" ? 1:0;
   unsigned int width = 0, height = 0;
   std::string error;
-  if (URIUtils::HasExtension(srcFile, ".jpg|.tbn") &&
-      GetCodingType(buf, width, height, orientation, error) &&
-      reenc.ReEncode(srcFile, buf, width, height, maxWidth, maxHeight, orientation, pDestBuffer, nDestSize))
+  if (URIUtils::HasExtension(srcFile, ".jpg|.tbn"))
   {
-    XFILE::CFile outfile;
-    if (outfile.OpenForWrite(destFile, true))
+    if (GetCodingType(buf, width, height, orientation, error))
     {
-      outfile.Write(pDestBuffer, nDestSize);
-      outfile.Close();
-      okay = true;
+      if (reenc.ReEncode(srcFile, buf, width, height, maxWidth, maxHeight, orientation, pDestBuffer, nDestSize))
+      {
+        XFILE::CFile outfile;
+        if (outfile.OpenForWrite(destFile, true))
+        {
+          outfile.Write(pDestBuffer, nDestSize);
+          outfile.Close();
+          okay = true;
+        }
+        else
+          CLog::Log(LOGERROR, "%s: can't open output file: %s\n", __func__, destFile.c_str());
+      }
+      else
+        CLog::Log(LOGERROR, "%s::%s %s %dx%d %s", CLASSNAME, __func__, srcFile.c_str(), width, height, "failed to encode");
     }
     else
-      CLog::Log(LOGERROR, "%s: can't open output file: %s\n", __func__, destFile.c_str());
+      CLog::Log(LOGDEBUG, "%s::%s %s %dx%d %s", CLASSNAME, __func__, srcFile.c_str(), width, height, error.c_str());
   }
   return okay;
 }
