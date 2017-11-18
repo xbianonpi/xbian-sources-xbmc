mk01/xbmc

[BaseRenderer] interlaced content displayed with interlaced resolution                must be displayed as is, without any zooming / stretching                etc.

xbmc/cores/VideoRenderers/BaseRenderer.cpp
@@ -587,6 +587,14 @@ void CBaseRenderer::CalcNormalDisplayRect(float offsetX, float offsetY, float sc
     return;
   }
 
+  RESOLUTION_INFO info = g_graphicsContext.GetResInfo(m_resolution);
+
+  if (info.dwFlags & D3DPRESENTFLAG_INTERLACED)
+  {
+    inputFrameRatio = (float) screenWidth / screenHeight;
+    zoomAmount = 1.0f;
+  }
+
   // scale up image as much as possible
   // and keep the aspect ratio (introduces with black bars)
   // calculate the correct output frame ratio (using the users pixel ratio setting

xbianonpi/xbian-sources-xbmc

xbmc/cores/VideoPlayer/VideoRenderers/BaseRenderer.cpp
@@ -221,6 +221,14 @@ void CBaseRenderer::CalcNormalRenderRect(float offsetX, float offsetY, float wid
     return;
   }
 
+  RESOLUTION_INFO info = g_graphicsContext.GetResInfo(m_resolution);
+
+  if (info.dwFlags & D3DPRESENTFLAG_INTERLACED)
+  {
+    inputFrameRatio = (float) width / height;
+    zoomAmount = 1.0f;
+  }
+
   // scale up image as much as possible
   // and keep the aspect ratio (introduces with black bars)
   // calculate the correct output frame ratio (using the users pixel ratio setting