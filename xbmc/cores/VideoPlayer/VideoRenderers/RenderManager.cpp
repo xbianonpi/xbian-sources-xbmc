9554eadf5270178369ff1c5dc9405213aba42ab0
popcornmix <popcornmix@gmail.com>
2018-05-29 22:31:57 +0100

    RenderManager: Revert show_video commit minimally

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -1101,9 +1101,6 @@ void CRenderManager::PrepareNextRender()
     return;
   }
 
-  if (!m_showVideo && !m_forceNext)
-    return;
-
   double frameOnScreen = m_dvdClock.GetClock();
   double frametime = 1.0 / CServiceBroker::GetWinSystem()->GetGfxContext().GetFPS() * DVD_TIME_BASE;
 
