c92264d7bc8be787598b798689577749bde7e11c
mkreisl <manfred.kreisl@nexgo.de>
2018-11-15 22:04:51 +0100

    [XBian] Modify FindJsonSchemaBuilder.cmake for finding built JsonSchemaBuilder under XBian build environment

--- cmake/modules/FindJsonSchemaBuilder.cmake
+++ cmake/modules/FindJsonSchemaBuilder.cmake
@@ -12,7 +12,7 @@
 #   JsonSchemaBuilder::JsonSchemaBuilder   - The JsonSchemaBuilder executable
 
 if(NOT TARGET JsonSchemaBuilder::JsonSchemaBuilder)
-  if(KODI_DEPENDSBUILD)
+  if(CMAKE_CROSSCOMPILING)
     add_executable(JsonSchemaBuilder::JsonSchemaBuilder IMPORTED GLOBAL)
     set_target_properties(JsonSchemaBuilder::JsonSchemaBuilder PROPERTIES
                                                        IMPORTED_LOCATION "${NATIVEPREFIX}/bin/JsonSchemaBuilder")
