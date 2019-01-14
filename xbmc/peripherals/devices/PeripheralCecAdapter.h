c52f852f1cade7f12fae9f99c8c415e3bffcd9d0
mkreisl <manfred.kreisl@nexgo.de>
2019-01-14 19:33:00 +0100

    [XBian extra] Commands cectogglestate, cecactivatesource, cecstandby enhanced:
    Accepts one optional parameter (1|force) to force execution of command
    This is required for putting TV into standby mode

--- xbmc/peripherals/devices/PeripheralCecAdapter.h
+++ xbmc/peripherals/devices/PeripheralCecAdapter.h
@@ -114,7 +114,7 @@ public:
 
   // public CEC methods
   void ActivateSource(void);
-  void StandbyDevices(void);
+  void StandbyDevices(bool forceStandby = false);
   bool ToggleDeviceState(CecStateChange mode = STATE_SWITCH_TOGGLE, bool forceType = false);
 
 private:
@@ -184,6 +184,7 @@ private:
   CEC::libcec_configuration m_configuration;
   bool m_bActiveSourcePending;
   bool m_bStandbyPending;
+  bool m_bStandbyForce;
   CDateTime m_preventActivateSourceOnPlay;
   bool m_bActiveSourceBeforeStandby;
   bool m_bOnPlayReceived;
@@ -224,3 +225,4 @@ protected:
 } // namespace PERIPHERALS
 
 #endif
+
