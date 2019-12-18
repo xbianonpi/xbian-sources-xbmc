d5a1edcc8a4e4ce6fb3833b74ee71c2fb0cb25cd
popcornmix <popcornmix@gmail.com>
2019-12-18 19:57:54 +0000

    windowing/gbm: Add display clock
    This allows use of "sync video to display"
    and features like resampling for smooth playback
    of 24fps content on 25fps display and manual
    speed adjustments

--- xbmc/windowing/gbm/WinSystemGbm.h
+++ xbmc/windowing/gbm/WinSystemGbm.h
@@ -69,6 +69,8 @@ public:
 protected:
   void OnLostDevice();
 
+  std::unique_ptr<CVideoSync> GetVideoSync(void* clock) override;
+
   std::shared_ptr<CDRMUtils> m_DRM;
   std::unique_ptr<CGBMUtils> m_GBM;
   std::shared_ptr<CVideoLayerBridge> m_videoLayerBridge;
