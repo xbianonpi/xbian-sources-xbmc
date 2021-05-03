e98ad7db1bfa147e406f09ad181b35abcd317839
mkreisl <manfred.kreisl@nexgo.de>
2021-05-04 01:11:24 +0200

    [DebugInfo] Add cpu usage info.

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -20,6 +20,7 @@
 #include "settings/Settings.h"
 #include "settings/SettingsComponent.h"
 #include "threads/SingleLock.h"
+#include "utils/CPUInfo.h"
 #include "utils/MathUtils.h"
 #include "utils/StringUtils.h"
 #include "utils/XTimeUtils.h"
@@ -746,6 +747,8 @@ void CRenderManager::Render(bool clear, DWORD flags, DWORD alpha, bool gui)
           info.vsync += StringUtils::Format("VSync: refresh:%.3f missed:%i speed:%.3f%%",
                                             refreshrate, missedvblanks, clockspeed * 100);
         }
+
+        info.cpu = CServiceBroker::GetCPUInfo()->GetCPUCount();
 
         m_debugRenderer.SetInfo(info);
       }
