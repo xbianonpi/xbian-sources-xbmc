13d3f47d9a51929cac5c49a2e73f4be4628d8375
mkreisl <manfred.kreisl@nexgo.de>
2021-12-22 15:39:27 +0100

    advancedsettings: Add option to set cache size on  libass
    E.g to set total cache size in libass to 32M

--- xbmc/settings/AdvancedSettings.h
+++ xbmc/settings/AdvancedSettings.h
@@ -339,6 +339,8 @@ class CAdvancedSettings : public ISettingCallback, public ISettingsHandler
     bool m_guiVideoLayoutTransparent{false};
     unsigned int m_addonPackageFolderSize;
 
+    unsigned int m_libAssCache;
+
     bool m_jsonOutputCompact;
     unsigned int m_jsonTcpPort;
 
