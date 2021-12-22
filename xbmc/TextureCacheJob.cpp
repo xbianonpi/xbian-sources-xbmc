fa9031ba7e385813e974d411f77dd16612924df3
mkreisl <manfred.kreisl@nexgo.de>
2021-12-22 15:46:44 +0100

    TextureCacheJob: Inline LoadImage

--- xbmc/TextureCacheJob.cpp
+++ xbmc/TextureCacheJob.cpp
@@ -144,7 +144,24 @@ bool CTextureCacheJob::CacheTexture(std::unique_ptr<CTexture>* out_texture)
   }
 #endif
 
-  std::unique_ptr<CTexture> texture = LoadImage(image, width, height, additional_info, true);
+  std::unique_ptr<CTexture> texture = nullptr;
+  // Validate file URL to see if it is an image
+  CFileItem file(image, false);
+  file.FillInMimeType();
+  if (!(!(file.IsPicture() && !(file.IsZIP() || file.IsRAR() || file.IsCBR() || file.IsCBZ() ))
+      && !StringUtils::StartsWithNoCase(file.GetMimeType(), "image/") && !StringUtils::EqualsNoCase(file.GetMimeType(), "application/octet-stream"))) // ignore non-pictures
+  {
+    texture = CTexture::LoadFromFile(image, width, height, true, file.GetMimeType());
+    if (texture)
+    {
+    // EXIF bits are interpreted as: <flipXY><flipY*flipX><flipX>
+    // where to undo the operation we apply them in reverse order <flipX>*<flipY*flipX>*<flipXY>
+    // When flipped we have an additional <flipX> on the left, which is equivalent to toggling the last bit
+    if (additional_info == "flipped")
+      texture->SetOrientation(texture->GetOrientation() ^ 1);
+    }
+  }
+
   if (texture)
   {
     if (texture->HasAlpha())
