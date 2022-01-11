5686cb37291bf202b2b4dd262b5e5f4ef0b16f9f
mkreisl <manfred.kreisl@nexgo.de>
2022-01-11 15:08:49 +0100

    RenderManager: Revert show_video commit minimally

--- xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/RenderManager.cpp
@@ -1125,9 +1125,6 @@ void CRenderManager::PrepareNextRender()
     return;
   }
 
-  if (!m_showVideo && !m_forceNext)
-    return;
-
   double frameOnScreen = m_dvdClock.GetClock();
   double frametime = 1.0 /
                      static_cast<double>(CServiceBroker::GetWinSystem()->GetGfxContext().GetFPS()) *
