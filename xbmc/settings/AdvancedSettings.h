fb8780b46d0a0d3183837130f94fd5a49201ad32
mkreisl <manfred.kreisl@nexgo.de>
2022-01-11 15:23:43 +0100

    Revert "AdvancedSettings: Cleanup audio settings not in use anymore"

--- xbmc/settings/AdvancedSettings.h
+++ xbmc/settings/AdvancedSettings.h
@@ -126,6 +126,8 @@ class CAdvancedSettings : public ISettingCallback, public ISettingsHandler
     static void GetCustomRegexps(TiXmlElement *pRootElement, std::vector<std::string> &settings);
     static void GetCustomExtensions(const TiXmlElement* pRootElement, std::string& extensions);
 
+    int m_audioHeadRoom;
+    float m_ac3Gain;
     std::string m_audioDefaultPlayer;
     float m_audioPlayCountMinimumPercent;
 #if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(61, 12, 100)
