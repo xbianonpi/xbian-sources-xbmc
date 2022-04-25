8094ea8f2ffe85b8da9febfc17d7a55294bbff58
mkreisl <manfred.kreisl@nexgo.de>
2022-04-25 18:04:07 +0200

    [XBian extra] Enable syslog and setlogtype
    - environment varible KODI_LOGTYPE=<type>. Set <type> of logging.
      type is either 0, 1, 2  or 3. See --logtype
    - commandline parameter --logtype=<type>. Set <type> of logging.
      type is either none|0, local|1, syslog|2 or both|3
      For example --logtype=2 enables logging via syslog
    - --syslog   Same as --logtype=2

--- xbmc/application/AppParams.h
+++ xbmc/application/AppParams.h
@@ -25,6 +25,9 @@ public:
   int GetLogLevel() const { return m_logLevel; }
   void SetLogLevel(int logLevel) { m_logLevel = logLevel; }
 
+  int GetLogType() const { return m_logType; }
+  void SetLogType(int logType) { m_logType = logType; }
+
   bool IsStartFullScreen() const { return m_startFullScreen; }
   void SetStartFullScreen(bool startFullScreen) { m_startFullScreen = startFullScreen; }
 
@@ -72,6 +75,7 @@ public:
 
 private:
   int m_logLevel{LOG_LEVEL_NORMAL};
+  int m_logType{1};
 
   bool m_startFullScreen{false};
   bool m_standAlone{false};
