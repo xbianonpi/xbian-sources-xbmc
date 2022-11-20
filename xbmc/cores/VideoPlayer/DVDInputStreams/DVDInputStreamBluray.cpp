mdodig/xbmc

[Settings] move SETTING_VIDEOPLAYER_SUPPORTMVC from platform settings to common settings.

xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -1172,7 +1172,7 @@ bool CDVDInputStreamBluray::ProcessItem(int playitem)
   
   m_title = m_dll->bd_get_playlist_info(m_bd, playitem, m_angle);
 
-  if (CSettings::GetInstance().GetBool("videoplayer.supportmvc") && !m_bMVCDisabled)
+  if (CSettings::GetInstance().GetBool(CSettings::SETTING_VIDEOPLAYER_SUPPORTMVC) && !m_bMVCDisabled)
   {
     MPLS_PL * mpls = m_dll->bd_get_title_mpls(m_bd);
     if (mpls)

xbianonpi/xbian-sources-xbmc

xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -1219,7 +1219,7 @@ bool CDVDInputStreamBluray::ProcessItem(int playitem)
 
   m_titleInfo = bd_get_playlist_info(m_bd, playitem, m_angle);
 
-  if (CSettings::GetInstance().GetBool("videoplayer.supportmvc") && !m_bMVCDisabled)
+  if (CServiceBroker::GetSettings().GetBool(CSettings::SETTING_VIDEOPLAYER_SUPPORTMVC) && !m_bMVCDisabled)
   {
     MPLS_PL * mpls = bd_get_title_mpls(m_bd);
     if (mpls)