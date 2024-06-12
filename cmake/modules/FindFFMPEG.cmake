7a459738e081fa889a100ecacf03117822d6b24c
mkreisl <manfred.kreisl@nexgo.de>
2024-06-12 17:02:32 +0200

    [XBian] Modify FindFFMPEG.cmake for working under XBian build

--- cmake/modules/FindFFMPEG.cmake
+++ cmake/modules/FindFFMPEG.cmake
@@ -48,13 +48,13 @@ macro(buildFFMPEG)
                              -DENABLE_VDPAU=${ENABLE_VDPAU}
                              -DEXTRA_FLAGS=${FFMPEG_EXTRA_FLAGS})
 
-  if(KODI_DEPENDSBUILD)
+  #if(KODI_DEPENDSBUILD)
     set(CROSS_ARGS -DDEPENDS_PATH=${DEPENDS_PATH}
                    -DPKG_CONFIG_EXECUTABLE=${PKG_CONFIG_EXECUTABLE}
                    -DCROSSCOMPILING=${CMAKE_CROSSCOMPILING}
                    -DOS=${OS}
                    -DCMAKE_AR=${CMAKE_AR})
-  endif()
+  #endif()
 
   if(USE_LTO)
     list(APPEND FFMPEG_OPTIONS -DUSE_LTO=ON)
